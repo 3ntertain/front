ba6cb77e773702dc2250aa0353a8a50e78467898
Manu <emm.bernard@gmail.com>
2023-08-09 08:54:07 +0200

    feat: Connect with backend api

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -13,9 +13,45 @@ export const AuthContextProvider = ({ children }) => {
   const [loading, setLoading] = React.useState(true)
 
   React.useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
       if (user) {
-        setUser(user)
+        const data = {
+          uid: user.uid,
+          email: user.email,
+        }
+
+        // Effectuez la requête POST à l'API en utilisant fetch
+        const response = await fetch(
+          process.env.NEXT_PUBLIC_apiUrl + "call-user/createOrConnect",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              // Vous pouvez également ajouter des en-têtes d'authentification si nécessaire
+            },
+            body: JSON.stringify(data),
+          }
+        )
+
+        // Vérifiez si la réponse est ok (statut 200-299)
+        if (!response.ok) {
+          throw new Error("La requête a échoué")
+        }
+
+        const jsonData = await response.json()
+
+        // const { completeUser } = {
+        //   user,
+        //   ...{ data: jsonData },
+        // }
+
+        const newUser = {
+          photoURL: user.photoURL,
+          displayName: user.displayName,
+          ...jsonData,
+        }
+
+        setUser(newUser)
       } else {
         setUser(null)
       }
